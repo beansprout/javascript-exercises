0f0d3e35a27a8693d4527ffc48584b345af86ff5
Christine Gierer <beansprout@users.noreply.github.com>
2019-03-16 18:08:00 -0400

    completed calculator

--- 04-calculator/calculator.js
+++ 04-calculator/calculator.js
@@ -1,25 +1,34 @@
-function add () {
-	
+function add (x,y) {
+  return x + y;
 }
 
-function subtract () {
-	
+function subtract (x,y) {
+  return x - y;
 }
 
-function sum () {
-	
+function sum (arr) {
+  if (arr.length === 0) {
+    return 0;
+  };
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  return arr.reduce((acc,curr) => (acc + curr));
 }
 
-function multiply () {
-	
+function multiply (arr) {
+  return arr.reduce((acc, curr) => (acc * curr));
 }
 
-function power() {
-	
+function power(x, n) {
+  return Math.pow(x, n);
 }
 
-function factorial() {
-	
+function factorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n-1);
 }
 
 module.exports = {
